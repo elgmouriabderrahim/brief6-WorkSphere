a37aec6615e83d564f5782c07a5ed914849cb050
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-11-17 21:10:36 +0000

    adding animation et reorganisation of aside

--- src/script.js
+++ src/script.js
@@ -2,10 +2,8 @@ const menu = document.querySelector('.menu');
 const aside = document.querySelector('aside');
 const x = document.querySelector('.x');
 menu.addEventListener("click", ()=>{
-    aside.classList.remove("hidden");
-    aside.classList.add("flex");
+    aside.classList.remove("translate-x-full");
 })
 x.addEventListener("click", ()=>{
-    aside.classList.add("hidden");
-    aside.classList.remove("flex");
+    aside.classList.add("translate-x-full");
 })
