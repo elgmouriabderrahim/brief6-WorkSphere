1f6b3b6ae19d33573f02755d1c00aeeac01c33cc
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-11-17 22:36:49 +0000

    create a pop up with animation

--- src/script.js
+++ src/script.js
@@ -1,9 +1,18 @@
 const menu = document.querySelector('.menu');
 const aside = document.querySelector('aside');
 const x = document.querySelector('.x');
+
+const modal = document.querySelector(".modal");
+const AddNewWorkerBtn = document.querySelector(".AddNewWorkerBtn");
+
+
 menu.addEventListener("click", ()=>{
     aside.classList.remove("translate-x-full");
 })
 x.addEventListener("click", ()=>{
     aside.classList.add("translate-x-full");
+})
+
+AddNewWorkerBtn.addEventListener("click", () =>{
+    modal.classList.add("opacity-100","scale-100","pointer-events-auto");
 })
