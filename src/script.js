99ff316bd9cf4a752fffe63bd6a29cc5b0c1d608
elgmouri abderrahim <ea.officialbox@gmail.com>
2025-11-18 17:56:42 +0000

    adding form

--- src/script.js
+++ src/script.js
@@ -5,6 +5,8 @@ const x = document.querySelector('.x');
 const modal = document.querySelector(".modal");
 const AddNewWorkerBtn = document.querySelector(".AddNewWorkerBtn");
 
+const cancel = document.querySelector(".cancel");
+const valider = document.querySelector(".valider");
 
 menu.addEventListener("click", ()=>{
     aside.classList.remove("translate-x-full");
@@ -14,5 +16,11 @@ x.addEventListener("click", ()=>{
 })
 
 AddNewWorkerBtn.addEventListener("click", () =>{
+    modal.classList.remove("scale-90");
     modal.classList.add("opacity-100","scale-100","pointer-events-auto");
+})
+
+
+cancel.addEventListener("click", ()=>{
+    modal.classList.remove("opacity-100","scale-100","pointer-events-auto");
 })
